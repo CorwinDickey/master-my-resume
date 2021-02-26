2351930343d0862c3c3be96131a2bf1292cd2e86
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-02-26 13:55:07 -0600

    Added deleteable boolean key to Section schema

--- models/section_model.js
+++ models/section_model.js
@@ -4,6 +4,7 @@ const sectionSchema = mongoose.Schema({
     name: String
     ,type: String
     ,items: Array
+    ,deletable: Boolean
 })
 
 const Section = mongoose.model('Section', sectionSchema)
