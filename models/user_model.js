d1824867e3f8ec0632b2a725c62331ec3f46a61b
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-02-26 09:39:37 -0600

    Created User model structure

---
+++ models/user_model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String
+        ,unique: true
+        ,required: true
+    }
+    ,password: {
+        type: String
+        ,minLength: 8
+    }
+    ,fullName: String
+    ,bio: String
+    ,sections: Array
+    ,items: Array
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
