88ca8d3d27d9c575f048b90325594acb5a0a4f98
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-03-02 14:55:01 -0600

    Added login functionality

---
+++ controllers/sessions_controller.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcrypt')
+const express = require('express')
+const ROUTER = express.Router()
+const User = require('../models/user_model.js')
+
+ROUTER.get('/new', (req, res) => {
+    res.render('sessions/new.ejs', {currentUser: req.session.currentUser})
+})
+
+ROUTER.post('/', (req, res) => {
+    // username is found and password matches
+    // successful log in
+
+    // username is not found - who cares about password if you don't have a username that is found?
+    // unsuccessful login
+
+    // username found but password doesn't match
+    // unsuccessful login
+
+    // some weird thing happened???
+
+    // Step 1, look for the username
+    User.findOne({username: req.body.username}, (err, foundUser) => {
+        // Database error
+        console.log(foundUser)
+        if (err) {
+            console.log(err)
+            res.send('oops, the db had a problem')
+        } else if (!foundUser) {
+            // if found user is undefined/null/not found etc
+            res.send('<a href="/">invalid username</a>')
+        } else {
+            // user is found
+            // now let's check if passwords match
+            if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+                // add hte user to our current session
+                req.session.currentUser = foundUser
+                // redirect back to our home page
+                res.redirect('/user/new')
+            } else {
+                // passwords do not match
+                res.send('<a href="/">invalid password</a>')
+            }
+        }
+    })
+})
+
+
+module.exports = ROUTER
