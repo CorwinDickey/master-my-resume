a0935a4865cc35ff2df5e651be0618c28a61768d
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-03-02 14:56:56 -0600

    Compartmentalized user creation functions into services file and removed them from controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,71 +5,8 @@ const bcrypt = require('bcrypt')
 const express = require('express')
 const ROUTER = express.Router()
 const User = require('../models/user_model.js')
-const Section = require('../models/section_model.js')
+const UserServices = require('../services/user_setup.js')
 // const { resolveInclude } = require('ejs')
-
-// ==============================================================
-// FUNCTIONS
-// ==============================================================
-
-// add basic sections to a new user profile
-async function createUserSections(userID) {
-    const newUserSections = [
-        {
-            name: 'Resumes'
-            ,type: 'resumes'
-            ,deletable: false
-            ,owner: userID
-        }
-        ,{
-            name: 'Work Experience'
-            ,type: 'block-item'
-            ,owner: userID
-        }
-        ,{
-            name: 'Projects'
-            ,type: 'block-item'
-            ,owner: userID
-        }
-        ,{
-            name: 'Skills'
-            ,type: 'list-item'
-            ,owner: userID
-        }
-        ,{
-            name: 'Achievements'
-            ,type: 'block-item'
-            ,owner: userID
-        }
-        ,{
-            name: 'Education'
-            ,type: 'block-item'
-            ,owner: userID
-        }
-    ]
-
-    function createSection(object) {
-        return new Promise((resolve, reject) => {
-            console.log('testing 3', object)
-            Section.create(object, (error, createdSection) => {
-                console.log(createdSection.id)
-                resolve(createdSection.id)
-            })
-            // resolve()
-        })
-    }
-
-    let sectionsArray = []
-    // console.log(newUserSections)
-    for (section of newUserSections) {
-        console.log(section)
-        let sectionID = await createSection(section)
-        console.log(sectionID)
-        sectionsArray.push(sectionID)
-    }
-    console.log(sectionsArray)
-    return sectionsArray
-}
 
 // ==============================================================
 // ROUTES
@@ -81,22 +18,19 @@ ROUTER.get('/new', (req, res) => {
 })
 
 // create new user
-ROUTER.post('/', (req, res) => {
+ROUTER.post('/', async (req, res) => {
     // create a new object with the user input (hash the password) and pass that to the database
     console.log('received a user creation request ===================================')
-    let newUserID = ''
-    let newUser = {
+    let userInfo = {
         email: req.body.email
         ,password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
     }
 
-    User.create(newUser, (error, createdUser) => {
-        // console.log(newUser)
-        console.log('user is created', createdUser)
-        newUserID = createdUser.id
-        // console.log(newUserID)
-        res.redirect('/user/' + newUserID + '/UserSetup')
-    })
+    console.log(userInfo)
+    let newUserObject = await UserServices.createUser(userInfo)
+    newUserID = newUserObject.id
+    // console.log(newUserID)
+    res.redirect('/user/' + newUserID + '/UserSetup')
 })
 
 // setup user account
@@ -104,7 +38,7 @@ ROUTER.get('/:id/UserSetup', async (req, res) => {
     User.findByIdAndUpdate(req.params.id, 
         {
             $set:{
-                sections: await createUserSections(req.params.id)
+                sections: await UserServices.addNewUserSections(req.params.id)
             }
         }, {new: true}, (error, newUser) => {
             // console.log(newUser)
---
+++ services/user_setup.js
@@ -0,0 +1,86 @@
+const User = require('../models/user_model.js')
+const Section = require('../models/section_model.js')
+
+const userSetup = {
+
+    createUser: async function(userInfo) {
+        // console.log(1, userInfo)
+        function createUser(userInfo) {
+            // console.log(2, userInfo)
+            return new Promise((resolve, reject) => {
+                // console.log(3, userInfo)
+                User.create(userInfo, (error, createdUser) => {
+                    if (error) {
+                        console.log(error)
+                    }
+                    console.log('user is created', createdUser)
+                    resolve(createdUser)
+                })
+            })
+        }
+        // console.log('Test 1')
+        let newUserObject = await createUser(userInfo)
+        // console.log(newUserObject)
+        return newUserObject
+    }
+    
+    // add basic sections to a new user profile
+    ,addNewUserSections: async function(userID) {
+        const newUserSections = [
+            {
+                name: 'Resumes'
+                ,type: 'resumes'
+                ,deletable: false
+                ,owner: userID
+            }
+            ,{
+                name: 'Work Experience'
+                ,type: 'block-item'
+                ,owner: userID
+            }
+            ,{
+                name: 'Projects'
+                ,type: 'block-item'
+                ,owner: userID
+            }
+            ,{
+                name: 'Skills'
+                ,type: 'list-item'
+                ,owner: userID
+            }
+            ,{
+                name: 'Achievements'
+                ,type: 'block-item'
+                ,owner: userID
+            }
+            ,{
+                name: 'Education'
+                ,type: 'block-item'
+                ,owner: userID
+            }
+        ]
+
+        function createSection(sectionObject) {
+            return new Promise((resolve, reject) => {
+                Section.create(sectionObject, (error, createdSection) => {
+                    console.log(createdSection)
+                    resolve(createdSection)
+                })
+                // resolve()
+            })
+        }
+
+        let sectionsArray = []
+        // console.log(newUserSections)
+        for (section of newUserSections) {
+            // console.log(section)
+            let newSection = await createSection(section)
+            // console.log(sectionID)
+            sectionsArray.push(newSection)
+        }
+        // console.log(sectionsArray)
+        return sectionsArray
+    }
+}
+
+module.exports = userSetup
