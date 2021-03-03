fa416ac65d445c04f2f2e5ce22e5960e974f449b
CorwinDickey <62188881+CorwinDickey@users.noreply.github.com>
2021-03-02 19:24:27 -0600

    Reorganized index and section pages

---
+++ controllers/section_items_controller.js
@@ -0,0 +1,47 @@
+// ==============================================================
+// CONTROLLER CONFIGURATION
+// ==============================================================
+const express = require('express')
+const ROUTER = express.Router()
+const BlockItem = require('../models/block_item_model.js')
+const ListItem = require('../models/list_item_model,js')
+
+// ==============================================================
+// ROUTES
+// ==============================================================
+
+// add new item
+ROUTER.get('/new', (req, res) => {
+    res.send('Testing new item route')
+})
+
+// create new item
+ROUTER.post('/', (req, res) => {
+    Section.create(req.body, (error, createdSection) => {})
+})
+
+ROUTER.get('/:id', (req, res) => {
+    Section.findById(req.param.id, (error, foundSection) => {
+        res.send(foundSection.name)
+    })
+})
+
+// edit item
+ROUTER.get('/:id/edit', (req, res) => {
+    res.send('Testing edit item route')
+})
+
+// update item
+ROUTER.put('/:id', (req, res) => {
+    console.log('Received an item update request')
+})
+
+// delete item
+ROUTER.delete('/:id', (req, res) => {
+    console.log('Received an item delete request')
+})
+
+// ==============================================================
+// EXPORTER
+// ==============================================================
+module.exports = ROUTER
